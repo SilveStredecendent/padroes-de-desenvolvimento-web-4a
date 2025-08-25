4457fce623b93126633585d4b451b9710ef74934
Herbert de Sousa Santoss <117468625+SilveStredecendent@users.noreply.github.com>
2025-08-25 23:52:03 +0000

    Criando tabelas

---
+++ projeto/src/main/java/entidades/Categoria.java
@@ -0,0 +1,15 @@
+package entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codigo;
+
+    private String nome;
+}
