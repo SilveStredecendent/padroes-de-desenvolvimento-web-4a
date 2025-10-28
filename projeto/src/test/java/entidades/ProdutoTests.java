68ba6de7fb60f96ae364c51f9808c336002528a8
Herbert de Sousa Santoss <117468625+SilveStredecendent@users.noreply.github.com>
2025-10-28 00:30:38 +0000

    fix(test): corrige testes de unidade de Produto

--- projeto/src/test/java/entidades/ProdutoTests.java
+++ projeto/src/test/java/entidades/ProdutoTests.java
@@ -1,26 +1,23 @@
 package entidades;
 
 import java.time.LocalDate;
-
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions; // Importe o Assertions
 import org.junit.jupiter.api.Test;
-
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.transaction.Transactional;
 
 @QuarkusTest
 public class ProdutoTests {
+
     @Test
     @Transactional
     public void testBuscaCategoriaBuscaUmFabricanteSalvaUmProduto() {
-
-        Categoria categoria = Categoria.findById(11);
+        
+        Categoria categoria = Categoria.findById(11);     
         Fabricante fabricante = Fabricante.findById(11);
 
         Produto produto = new Produto();
-
         produto.nome = "Macarrão Espaguete 500Gm";
-
         produto.preco = 9.00;
         produto.quantidade = 30;
         produto.validade = LocalDate.of(2026, 4, 30);
@@ -28,7 +25,11 @@ public class ProdutoTests {
         produto.fabricante = fabricante;
 
         produto.persist();
-        Assertions.assertEquals(12, produto.codigo);
-    }
 
+        
+        Assertions.assertNotNull(produto.codigo);
+
+        Assertions.assertEquals("Macarrão Espaguete 500Gm", produto.nome);
+        Assertions.assertEquals("Massas", produto.categoria.nome);
+    }
 }
